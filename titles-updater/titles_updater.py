9d3c4abd4fe34576852941ebe3a9eca56e6b2add
fulder <misad90@gmail.com>
2019-05-31 00:14:36 +0200

    Sleep inside python and remove old titles

--- titles-updater/titles_updater.py
+++ titles-updater/titles_updater.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+import time
 
 import requests
 
@@ -8,26 +9,39 @@ from config.config import Config
 titles_path = os.path.join(Config().cache_dir, "titles")
 os.makedirs(titles_path, exist_ok=True)
 
-now = datetime.datetime.now()
-current_filename = os.path.join(titles_path, "{}.xml".format(now.strftime("%Y-%m-%d")))
+sleep_time = 100
 
-if not os.path.isfile(current_filename):
-    print("Downloading new titles file")
-    r = requests.get("http://anidb.net/api/anime-titles.xml.gz", stream=True)
+while True:
+    now = datetime.datetime.now()
+    date_today = now.strftime("%Y-%m-%d")
+    date_yesterday = (now - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+    current_filename = os.path.join(titles_path, "{}.xml".format(date_today))
 
-    total_size = int(r.headers.get('content-length', 0))
-    count = 0
-    with open(current_filename, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=1024):
-            if chunk:  # filter out keep-alive new chunks
-                f.write(chunk)
-                count += 1
-                if count % 100 == 0:
-                    print("Progress in percentage: {}".format((1024 * count) / float(total_size)))
+    if not os.path.isfile(current_filename):
+        print("Downloading new titles file")
+        r = requests.get("http://anidb.net/api/anime-titles.xml.gz", stream=True)
 
-    print("Progress in percentage: 1")
-else:
-    print("Title file for today already exists")
+        total_size = int(r.headers.get('content-length', 0))
+        count = 0
+        with open(current_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024):
+                if chunk:  # filter out keep-alive new chunks
+                    f.write(chunk)
+                    count += 1
+                    if count % 100 == 0:
+                        print("Progress in percentage: {}".format((1024 * count) / float(total_size)))
 
-# A normal xml file not even gunzipped!
-print("Current titles: {}".format(os.path.abspath(current_filename)))
+        print("Progress in percentage: 1")
+    else:
+        print("Title file for today already exists")
+
+    print("Current titles: {}".format(os.path.abspath(current_filename)))
+
+    # Clean up all old titles more than 2 days old
+    for title in os.listdir(titles_path):
+        if title != "{}.xml".format(date_today) and title != "{}.xml".format(date_yesterday):
+            print("Removing old titles file: {}", title)
+            os.remove(os.path.join(titles_path, title))
+
+    print("Sleeping %s seconds" % sleep_time)
+    time.sleep(sleep_time)
