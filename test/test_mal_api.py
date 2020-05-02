5559850bb756438632f84388363e9f03a92ff076
fulder <misad90@gmail.com>
2020-05-02 13:41:05 +0200

    Add happy test for getting anime

--- test/test_mal_api.py
+++ test/test_mal_api.py
@@ -54,3 +54,24 @@ def test_search_invalid_status(req_mock):
 
     assert "Unexpected status code: 500" == str(e.value)
 
+
+@mock.patch.dict(os.environ, ENV)
+@patch.object(requests, "get")
+def test_get_anime(req_mock):
+    exp = {
+        "anime": {
+            "id": 21,
+            "title": "One Piece",
+            "main_picture": {
+                "medium": "https:\/\/api-cdn.myanimelist.net\/images\/anime\/6\/73245.jpg",
+                "large": "https:\/\/api-cdn.myanimelist.net\/images\/anime\/6\/73245l.jpg"
+            }
+        }
+    }
+
+    req_mock.return_value.status_code = 200
+    req_mock.return_value.json.return_value = exp["anime"]
+
+    mal_api = MalApi()
+    ret = mal_api.get_anime(21)
+    assert ret == exp
