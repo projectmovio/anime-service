0d6040476a32559fcd240c2a36259354c66bbd43
fulder <misad90@gmail.com>
2020-05-02 13:11:13 +0200

    Catch MAL exceptions in get_anime lambda

--- src/get_anime/__init__.py
+++ src/get_anime/__init__.py
@@ -1,6 +1,10 @@
 import json
 
-from mal import MalApi
+from mal import MalApi, NotFoundError
+
+
+class HttpError(object):
+    pass
 
 
 def handle(event, context):
@@ -17,13 +21,27 @@ def handle(event, context):
     mid = event["queryStringParameters"].get("mid")
 
     if mid:
-        res = mal_api.get_anime(mid)
+        try:
+            res = mal_api.get_anime(mid)
+        except NotFoundError:
+            return {
+                "statusCode": 404
+            }
+        except HttpError:
+            return {
+                "statusCode": 500
+            }
         return {
             "statusCode": 200,
             "body": json.dumps(res)
         }
     elif search:
-        res = mal_api.search(search)
+        try:
+            res = mal_api.search(search)
+        except HttpError:
+            return {
+                "statusCode": 500
+            }
         return {
             "statusCode": 200,
             "body": json.dumps(res)
