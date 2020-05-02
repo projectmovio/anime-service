9cdbe2cd4cb379e92a6e9333e8f49b8b8d97e21f
fulder <misad90@gmail.com>
2020-05-02 20:59:28 +0200

    Start implementing anime database layer

---
+++ src/layers/databases/python/anime.py
@@ -0,0 +1,38 @@
+import os
+import uuid
+
+import boto3
+from boto3.dynamodb.conditions import Key
+
+DATABASE_NAME = os.getenv("ANIME_DATABASE_NAME")
+
+table = None
+
+
+def _get_table():
+    global table
+    if table is None:
+        table = boto3.resource("dynamodb").Table(DATABASE_NAME)
+    return table
+
+
+def new_anime(mal_id):
+    anime_id = uuid.uuid5(uuid.NAMESPACE_OID, mal_id)
+
+    _get_table().update_item(
+        Key={"id": anime_id},
+        UpdateExpression="SET #attr1 = :val1",
+        ExpressionAttributeNames={"#attr1": "mal_id"},
+        ExpressionAttributeValues={":val1": mal_id}
+    )
+
+    return anime_id
+
+
+def get_anime(mal_id):
+    res = _get_table().query(
+        IndexName='mal_id',
+        KeyConditionExpression=Key('mal_id').eq(mal_id)
+    )
+
+    return res["Items"][0]
