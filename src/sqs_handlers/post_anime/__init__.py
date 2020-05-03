70de23355d6b1011ba5801edd5d7929ad69219ca
fulder <misad90@gmail.com>
2020-05-03 12:26:27 +0200

    Start adding sqs_handler for post_anime

---
+++ src/sqs_handlers/post_anime/__init__.py
@@ -0,0 +1,19 @@
+import time
+
+import params
+
+
+def handler(event, context):
+    last_timestamp = params.get_last_post_anime_update()
+
+    current_timestamp = int(time.time())
+    if current_timestamp - last_timestamp <= 2:
+        print("Previous update triggered less than 2 seconds ago")
+        time.sleep(2)
+
+    # batch size always 1, sleep and throttle could increase runtime close to lambda timeout
+    # divide it in 1 update per lambda instead. This will also make the code cleaner.
+    mal_id = event["Records"][0]["body"]
+
+
+    params.set_last_post_anime_update(int(time.time()), mal_id)
