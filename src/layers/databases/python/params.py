61bdb1a689af94080e7b75b3dbd6c173e13ef910
fulder <misad90@gmail.com>
2020-05-03 11:56:43 +0200

    Add params database

---
+++ src/layers/databases/python/params.py
@@ -0,0 +1,41 @@
+import os
+
+import boto3
+
+DATABASE_NAME = os.getenv("ANIME_PARAMS_DATABASE_NAME")
+
+table = None
+
+
+class Error(Exception):
+    pass
+
+
+class NotFoundError(Error):
+    pass
+
+
+def _get_table():
+    global table
+    if table is None:
+        table = boto3.resource("dynamodb").Table(DATABASE_NAME)
+    return table
+
+
+def set_last_post_anime_update(timestamp, mal_id, anidb_id):
+    _get_table().update_item(
+        Key={"name": "post_anime_update"},
+        UpdateExpression="SET timestamp= :timestamp, mal_id = :mal_id, anidb_id = :anidb_id",
+        ExpressionAttributeValues={":timestamp": timestamp, ":mal_id": mal_id, ":anidb_id": anidb_id}
+    )
+
+
+def get_last_post_anime_update():
+    res = _get_table().get_item(
+        Key={"name": "post_anime_update"}
+    )
+
+    if "Item" in res:
+        return res["Item"]["timestamp"]
+
+    return None
