9ff7214e934821a1521e4b2a10ff195b5aaf0478
fulder <misad90@gmail.com>
2020-05-16 13:01:37 +0200

    Check DB in get_anime before calling API

--- src/api/get_anime/__init__.py
+++ src/api/get_anime/__init__.py
@@ -1,7 +1,7 @@
 import json
 
 from mal import MalApi, NotFoundError
-
+import anime_db
 
 class HttpError(object):
     pass
@@ -19,18 +19,25 @@ def handle(event, context):
             "body": json.dumps({"error": "Please specify either 'search' or 'mal_id' query parameter"})
         }
 
-    mal_api = MalApi()
     if mal_id:
         try:
-            res = mal_api.get_anime(mal_id)
+            res = anime_db.get_anime(mal_id)
+        except anime_db.NotFoundError:
+            print(f"Anime with mal_id: {mal_id} not found in DB, use API")
+        else:
+            return {"statusCode": 200, "body": json.dumps(res)}
+
+        try:
+            res = MalApi().get_anime(mal_id)
         except NotFoundError:
             return {"statusCode": 404}
         except HttpError:
             return {"statusCode": 500}
-        return {"statusCode": 200, "body": json.dumps(res)}
+        else:
+            return {"statusCode": 200, "body": json.dumps(res)}
     elif search:
         try:
-            res = mal_api.search(search)
+            res = MalApi().search(search)
         except HttpError:
             return {"statusCode": 500}
         return {"statusCode": 200, "body": json.dumps(res)}
