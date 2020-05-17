a0a91d0955ca80cc7d678d06deb79198a01b5ff4
fulder <misad90@gmail.com>
2020-05-17 17:00:38 +0200

    Add db layer for episodes table

---
+++ src/layers/databases/python/episodes_db.py
@@ -0,0 +1,31 @@
+import os
+import uuid
+
+import boto3
+from boto3.dynamodb.conditions import Key
+
+DATABASE_NAME = os.getenv("ANIME_EPISODES_DATABASE_NAME")
+
+table = None
+
+
+class Error(Exception):
+    pass
+
+
+class NotFoundError(Error):
+    pass
+
+
+def _get_table():
+    global table
+    if table is None:
+        table = boto3.resource("dynamodb").Table(DATABASE_NAME)
+    return table
+
+
+def put_episodes(anime_id, episodes):
+    with _get_table().batch_writer() as batch:
+        for ep in episodes:
+            ep["anime_id"] = anime_id
+            batch.put_item(Item=ep)
