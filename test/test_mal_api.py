84e5afcd08bc6a6d8e0f97312dfd52916f5ba66f
fulder <misad90@gmail.com>
2020-06-06 22:33:21 +0200

    Split up base anime test item to constant

--- test/test_mal_api.py
+++ test/test_mal_api.py
@@ -9,6 +9,19 @@ from mal import HTTPError, MalApi, NotFoundError, Anime
 
 ENV = {"MAL_CLIENT_ID": "TEST_MAL_CLIENT_ID"}
 
+BASE_NARUTO_ANIME = {
+    "id": 20,
+    "title": "Naruto",
+    "main_picture": {
+        "medium": "https://api-cdn.myanimelist.net/images/anime/13/17405.jpg",
+        "large": "https://api-cdn.myanimelist.net/images/anime/13/17405l.jpg"
+    },
+    "media_type": "tv",
+    "num_episodes": 220,
+    "average_episode_duration": 1404,
+    "synopsis": "Moments prior to Naruto Uzumaki's birth, a huge demon known as the Kyuubi, the Nine-Tailed Fox, attacked Konohagakure, the Hidden Leaf Village, and wreaked havoc. In order to put an end to the Kyuubi's rampage, the leader of the village, the Fourth Hokage, sacrificed his life and sealed the monstrous beast inside the newborn Naruto.\n\nNow, Naruto is a hyperactive and knuckle-headed ninja still living in Konohagakure. Shunned because of the Kyuubi inside him, Naruto struggles to find his place in the village, while his burning desire to become the Hokage of Konohagakure leads him not only to some great new friends, but also some deadly foes.\n\n[Written by MAL Rewrite]",
+}
+
 
 @mock.patch.dict(os.environ, ENV)
 @patch.object(requests, "get")
@@ -86,17 +99,7 @@ def test_get_anime_error(req_mock):
 
 
 def test_get_all_titles():
-    anime = Anime(**{
-        "id": 20,
-        "title": "Naruto",
-        "main_picture": {
-            "medium": "https:\/\/api-cdn.myanimelist.net\/images\/anime\/13\/17405.jpg",
-            "large": "https:\/\/api-cdn.myanimelist.net\/images\/anime\/13\/17405l.jpg"
-        },
-        "media_type": "tv",
-        "num_episodes": 220,
-        "average_episode_duration": 1404,
-        "synopsis": "Moments prior to Naruto Uzumaki's birth, a huge demon known as the Kyuubi, the Nine-Tailed Fox, attacked Konohagakure, the Hidden Leaf Village, and wreaked havoc. In order to put an end to the Kyuubi's rampage, the leader of the village, the Fourth Hokage, sacrificed his life and sealed the monstrous beast inside the newborn Naruto.\n\nNow, Naruto is a hyperactive and knuckle-headed ninja still living in Konohagakure. Shunned because of the Kyuubi inside him, Naruto struggles to find his place in the village, while his burning desire to become the Hokage of Konohagakure leads him not only to some great new friends, but also some deadly foes.\n\n[Written by MAL Rewrite]",
+    anime = Anime(**BASE_NARUTO_ANIME, **{
         "alternative_titles": {
             "synonyms": [
                 "NARUTO"
@@ -117,4 +120,8 @@ def test_get_all_titles():
     assert exp == all_titles
 
 
+def test_start_date():
+    anime = Anime(**BASE_NARUTO_ANIME, **{
+        "start_date": "2020-01-01"
+    })
 
