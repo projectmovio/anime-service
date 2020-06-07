21d9028672f10cb6429b01b6ef47743c1cf7df67
fulder <misad90@gmail.com>
2020-06-07 12:06:38 +0200

    Add types to dataclass_from_dict

--- src/layers/common/python/utils.py
+++ src/layers/common/python/utils.py
@@ -1,7 +1,10 @@
 from dataclasses import fields
+from typing import TypeVar, Type
+
+T = TypeVar('T')
 
 
-def dataclass_from_dict(dataclass, d):
+def dataclass_from_dict(dataclass: Type[T], d: dict) -> T:
     try:
         fieldtypes = {f.name: f.type for f in fields(dataclass)}
         return dataclass(**{f: dataclass_from_dict(fieldtypes[f], d[f]) for f in d})
