24af8f4c0830612f342bcadbc29541a7cb606bdb
fulder <misad90@gmail.com>
2020-06-07 11:41:06 +0200

    Raise error if dataclass_from_dict fails

--- src/layers/common/python/utils.py
+++ src/layers/common/python/utils.py
@@ -4,7 +4,8 @@ from dataclasses import fields
 def dataclass_from_dict(dataclass, d):
     try:
         fieldtypes = {f.name: f.type for f in fields(dataclass)}
-        print(fieldtypes)
         return dataclass(**{f: dataclass_from_dict(fieldtypes[f], d[f]) for f in d})
-    except:
-        return d
+    except TypeError as e:
+        if str(e) == "must be called with a dataclass type or instance":
+            return d
+        raise
