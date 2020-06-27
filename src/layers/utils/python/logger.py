511b578fa0527a02d65284bd0cad7dec1caf0efc
fulder <misad90@gmail.com>
2020-06-27 17:53:34 +0200

    Add utils layer with logger utils

---
+++ src/layers/utils/python/logger.py
@@ -0,0 +1,19 @@
+import logging
+import os
+
+from pythonjsonlogger import jsonlogger
+
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(LOG_LEVEL)
+
+    handler = logging.StreamHandler()
+    formatter = jsonlogger.JsonFormatter(fmt='%(asctime)s %(levelname)s %(name)s %(message)s')
+    handler.setFormatter(formatter)
+    handler.setLevel(LOG_LEVEL)
+    logger.addHandler(handler)
+
+    return logger
