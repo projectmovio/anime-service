a14d7d48b864574bdca31a68c69b4a14f7272222
fulder <misad90@gmail.com>
2020-06-29 00:24:10 +0200

    Install layers requirements

--- deploy/lib/layers.py
+++ deploy/lib/layers.py
@@ -1,10 +1,13 @@
 import os
+import shutil
+import subprocess
 
 from aws_cdk import core
 from aws_cdk.aws_lambda import LayerVersion, Code, Runtime
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 LAYERS_DIR = os.path.join(CURRENT_DIR, "..", "..", "src", "layers")
+BUILD_FOLDER = os.path.join(CURRENT_DIR, "..", "..", "build")
 
 
 class Layers(core.Stack):
@@ -15,10 +18,25 @@ class Layers(core.Stack):
         self._create_layers()
 
     def _create_layers(self):
+        if os.path.isdir(BUILD_FOLDER):
+            shutil.rmtree(BUILD_FOLDER)
+        os.mkdir(BUILD_FOLDER)
+
         for layer in os.listdir(LAYERS_DIR):
+            layer_folder = os.path.join(LAYERS_DIR, layer)
+            build_folder = os.path.join(BUILD_FOLDER, layer)
+            shutil.copytree(layer_folder, build_folder)
+
+            requirements_path = os.path.join(build_folder, "requirements.txt")
+
+            if os.path.isfile(requirements_path):
+                packages_folder = os.path.join(build_folder, "python", "lib", "python3.8", "site-packages")
+                print(f"Installing layer requirements to target: {os.path.abspath(packages_folder)}")
+                subprocess.check_output(["pip", "install", "-r", requirements_path, "-t", packages_folder])
+
             self.layers[layer] = LayerVersion(
                 self,
                 layer,
-                code=Code.from_asset(path=os.path.join(LAYERS_DIR, layer)),
+                code=Code.from_asset(path=build_folder),
                 compatible_runtimes=[Runtime.PYTHON_3_8],
             )
