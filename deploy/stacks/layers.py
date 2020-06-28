5dfc81b8d6cc6cd24c902831901bfc5b5fefb5d9
fulder <misad90@gmail.com>
2020-06-28 23:04:08 +0200

    Start implementing layers stack, add API layer

--- deploy/stacks/layers.py
+++ deploy/stacks/layers.py
@@ -1,7 +1,22 @@
+import os
+
 from aws_cdk import core
+from aws_cdk.aws_lambda import LayerVersion, Code, Runtime
+
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+LAYERS_DIR = os.path.join(CURRENT_DIR, "..", "..", "src", "layers")
 
 
 class Layers(core.Stack):
 
     def __init__(self, app: core.App, id: str, **kwargs) -> None:
         super().__init__(app, id, **kwargs)
+        self._create_layers()
+
+    def _create_layers(self):
+        LayerVersion(
+            self,
+            "api",
+            code=Code.from_asset(path=os.path.join(LAYERS_DIR, "api")),
+            compatible_runtimes=[Runtime.PYTHON_3_8],
+        )
