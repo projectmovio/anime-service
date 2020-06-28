b9c576782ec2126b2a6ab76c79d9c8e71d32c020
fulder <misad90@gmail.com>
2020-06-28 23:28:34 +0200

    Clean pycache before looping through lambdas

--- deploy/cdk.py
+++ deploy/cdk.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
-
 from aws_cdk import core
 
-from stacks.layers import Layers
+from lib.lambdas import Lambdas
+from lib.layers import Layers
+from lib.utils import clean_pycache
+
+clean_pycache()
 
 app = core.App()
 
 env = {"region": "eu-west-1"}
 
-Layers(app, "layers", env=env)
+Layers(app, "anime-layers", env=env)
+Lambdas(app, "anime-lambdas", env=env)
 
 app.synth()
