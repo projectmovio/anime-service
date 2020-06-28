9268d06805d45e777798d905675927d5200e4bba
fulder <misad90@gmail.com>
2020-06-28 12:20:47 +0200

    Add test for titles_updater when no S3 file exist

--- test/test_titles_updater.py
+++ test/test_titles_updater.py
@@ -1,7 +1,9 @@
 import filecmp
+import gzip
 import json
 import os
 import shutil
+from unittest import mock
 from unittest.mock import MagicMock
 
 import pytest
@@ -34,6 +36,30 @@ def test_handler(mocked_anidb):
 
     assert filecmp.cmp(xml_path, TEST_TITLES)
 
-    exp_json_titles = {'CotS': 1, 'Crest of the Stars': 1, 'Hvězdný erb': 1, 'Seikai no Monshou': 1, 'SnM': 1, '星界の紋章': 1, '星界之纹章': 1}
+    exp_json_titles = {'CotS': 1, 'Crest of the Stars': 1, 'Hvězdný erb': 1, 'Seikai no Monshou': 1, 'SnM': 1,
+                       '星界の紋章': 1, '星界之纹章': 1}
+    with open(json_path) as fs:
+        assert exp_json_titles == json.load(fs)
+
+
+@mock.patch("requests.get")
+def test_handler_no_s3_file(mocked_get, mocked_anidb):
+    mocked_anidb.s3_bucket.download_file.return_value = False
+    mocked_anidb.s3_bucket.upload_file.return_value = True
+
+    # Create test gzip
+    titles_gzip = os.path.join(CURRENT_DIR, "test_titles.gz")
+    with gzip.open(titles_gzip, 'wb') as gz:
+        with open(TEST_TITLES, "rb") as f:
+            gz.write(f.read())
+
+    mocked_get.return_value = titles_gzip
+
+    xml_path, json_path = handle(None, None)
+
+    assert filecmp.cmp(xml_path, TEST_TITLES)
+
+    exp_json_titles = {'CotS': 1, 'Crest of the Stars': 1, 'Hvězdný erb': 1, 'Seikai no Monshou': 1, 'SnM': 1,
+                       '星界の紋章': 1, '星界之纹章': 1}
     with open(json_path) as fs:
         assert exp_json_titles == json.load(fs)
