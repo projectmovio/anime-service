86133349fe4f949e2e5e2a43db00b5ad677569c0
fulder <misad90@gmail.com>
2020-06-28 23:28:14 +0200

    Rename stacks to lib

--- deploy/lib/layers.py
+++ deploy/lib/layers.py
@@ -11,8 +11,8 @@ class Layers(core.Stack):
 
     def __init__(self, app: core.App, id: str, **kwargs) -> None:
         super().__init__(app, id, **kwargs)
-        self._create_layers()
         self.layers = {}
+        self._create_layers()
 
     def _create_layers(self):
         for layer in os.listdir(LAYERS_DIR):
