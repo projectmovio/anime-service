3bab42384af3ab0e91368366e710c6e85188e3ce
fulder <misad90@gmail.com>
2020-07-17 13:41:28 +0200

    Add tests for invalid start and limit

--- test/unittest/test_anime_episodes.py
+++ test/unittest/test_anime_episodes.py
@@ -92,6 +92,7 @@ def test_handler_with_limit(mocked_episodes_db):
     }
     assert res == exp
 
+
 def test_handler_with_start(mocked_episodes_db):
     exp_eps = [
         {
@@ -161,5 +162,79 @@ def test_handler_with_limit_and_start(mocked_episodes_db):
     exp = {
         "statusCode": 200,
         "body": json.dumps([exp_eps[1]])
+    }
+    assert res == exp
+
+
+def test_handler_with_invalid_limit_type(mocked_episodes_db):
+    exp_eps = [
+        {
+            "id": "1",
+            "title": "ep1",
+        },
+        {
+            "id": "2",
+            "title": "ep2",
+        }
+    ]
+    m = MagicMock()
+    mocked_episodes_db.client.get_paginator.return_value = m
+    m.paginate.return_value = [
+        {"Items": [exp_eps[0]]},
+        {"Items": [exp_eps[1]]}
+    ]
+
+    event = {
+        "pathParameters": {
+            "anime_id": "123"
+        },
+        "queryStringParameters": {
+            "limit": "abc",
+            "start": 2
+        }
+    }
+
+    res = handle(event, None)
+
+    exp = {
+        "statusCode": 400,
+        "body": json.dumps({"message": "Invalid limit type"})
+    }
+    assert res == exp
+
+
+def test_handler_with_invalid_start_type(mocked_episodes_db):
+    exp_eps = [
+        {
+            "id": "1",
+            "title": "ep1",
+        },
+        {
+            "id": "2",
+            "title": "ep2",
+        }
+    ]
+    m = MagicMock()
+    mocked_episodes_db.client.get_paginator.return_value = m
+    m.paginate.return_value = [
+        {"Items": [exp_eps[0]]},
+        {"Items": [exp_eps[1]]}
+    ]
+
+    event = {
+        "pathParameters": {
+            "anime_id": "123"
+        },
+        "queryStringParameters": {
+            "limit": 1,
+            "start": "abc"
+        }
+    }
+
+    res = handle(event, None)
+
+    exp = {
+        "statusCode": 400,
+        "body": json.dumps({"message": "Invalid start type"})
     }
     assert res == exp
