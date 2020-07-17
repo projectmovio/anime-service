1dea3ee254f21ce00d3074e52579c392bc174969
fulder <misad90@gmail.com>
2020-07-17 19:59:26 +0200

    Start implementing apitests

---
+++ test/apitest/test_anime.py
@@ -0,0 +1,71 @@
+import time
+import uuid
+
+import requests
+
+from apitest.conftest import API_URL, BASE_HEADERS
+
+
+def test_search():
+    res = requests.get(f"{API_URL}/anime?search=Naruto", headers=BASE_HEADERS)
+
+    assert res.status_code == 200
+    item = res.json()[0]
+    assert item["id"] == 20
+    assert item["title"] == "Naruto"
+    time.sleep(1)
+
+
+def test_search_by_mal_id():
+    res = requests.get(f"{API_URL}/anime?mal_id=20", headers=BASE_HEADERS)
+
+    assert res.status_code == 200
+    item = res.json()
+    assert item["mal_id"] == 20
+    assert item["title"] == "Naruto"
+
+
+def test_get_anime_by_id():
+    anime_id = str(uuid.uuid5(uuid.NAMESPACE_OID, "20"))
+    res = requests.get(f"{API_URL}/anime/{anime_id}", headers=BASE_HEADERS)
+
+    assert res.status_code == 200
+    item = res.json()
+    assert item["mal_id"] == 20
+    assert item["title"] == "Naruto"
+
+
+def test_get_anime_episodes():
+    anime_id = str(uuid.uuid5(uuid.NAMESPACE_OID, "20"))
+    res = requests.get(f"{API_URL}/anime/{anime_id}/episodes", headers=BASE_HEADERS)
+
+    assert res.status_code == 200
+    item = res.json()
+    assert len(item) == 100
+    assert item[0]["episode_number"] == "220"
+    assert item[-1]["episode_number"] == "121"
+    time.sleep(1)
+
+
+def test_get_anime_episodes_with_changed_limit():
+    anime_id = str(uuid.uuid5(uuid.NAMESPACE_OID, "20"))
+    res = requests.get(f"{API_URL}/anime/{anime_id}/episodes?limit=10", headers=BASE_HEADERS)
+
+    assert res.status_code == 200
+    item = res.json()
+    assert len(item) == 10
+    assert item[0]["episode_number"] == "220"
+    assert item[-1]["episode_number"] == "211"
+    time.sleep(1)
+
+
+def test_get_anime_episodes_with_changed_start_and_limit():
+    anime_id = str(uuid.uuid5(uuid.NAMESPACE_OID, "20"))
+    res = requests.get(f"{API_URL}/anime/{anime_id}/episodes?limit=10&start=100", headers=BASE_HEADERS)
+
+    assert res.status_code == 200
+    item = res.json()
+    assert len(item) == 10
+    assert item[0]["episode_number"] == "100"
+    assert item[-1]["episode_number"] == "91"
+    time.sleep(1)
