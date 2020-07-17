42824af4f4b79f2f5eb54776c4e0fb8d2b9d7c1d
fulder <misad90@gmail.com>
2020-07-17 12:27:47 +0200

    Add tests for start,limit and start with limit

--- test/unittest/test_anime_episodes.py
+++ test/unittest/test_anime_episodes.py
@@ -55,3 +55,111 @@ def test_handler_not_found(mocked_episodes_db):
         "statusCode": 404,
     }
     assert res == exp
+
+
+def test_handler_with_limit(mocked_episodes_db):
+    exp_eps = [
+        {
+            "id": "1",
+            "title": "ep1",
+        },
+        {
+            "id": "2",
+            "title": "ep2",
+        }
+    ]
+    m = MagicMock()
+    mocked_episodes_db.client.get_paginator.return_value = m
+    m.paginate.return_value = [
+        {"Items": [exp_eps[0]]},
+        {"Items": [exp_eps[1]]}
+    ]
+
+    event = {
+        "pathParameters": {
+            "anime_id": "123"
+        },
+        "queryStringParameters": {
+            "limit": 1
+        }
+    }
+
+    res = handle(event, None)
+
+    exp = {
+        "statusCode": 200,
+        "body": json.dumps([exp_eps[0]])
+    }
+    assert res == exp
+
+def test_handler_with_start(mocked_episodes_db):
+    exp_eps = [
+        {
+            "id": "1",
+            "title": "ep1",
+        },
+        {
+            "id": "2",
+            "title": "ep2",
+        }
+    ]
+    m = MagicMock()
+    mocked_episodes_db.client.get_paginator.return_value = m
+    m.paginate.return_value = [
+        {"Items": [exp_eps[0]]},
+        {"Items": [exp_eps[1]]}
+    ]
+
+    event = {
+        "pathParameters": {
+            "anime_id": "123"
+        },
+        "queryStringParameters": {
+            "start": 2
+        }
+    }
+
+    res = handle(event, None)
+
+    exp = {
+        "statusCode": 200,
+        "body": json.dumps([exp_eps[1]])
+    }
+    assert res == exp
+
+
+def test_handler_with_limit_and_start(mocked_episodes_db):
+    exp_eps = [
+        {
+            "id": "1",
+            "title": "ep1",
+        },
+        {
+            "id": "2",
+            "title": "ep2",
+        }
+    ]
+    m = MagicMock()
+    mocked_episodes_db.client.get_paginator.return_value = m
+    m.paginate.return_value = [
+        {"Items": [exp_eps[0]]},
+        {"Items": [exp_eps[1]]}
+    ]
+
+    event = {
+        "pathParameters": {
+            "anime_id": "123"
+        },
+        "queryStringParameters": {
+            "limit": 1,
+            "start": 2
+        }
+    }
+
+    res = handle(event, None)
+
+    exp = {
+        "statusCode": 200,
+        "body": json.dumps([exp_eps[1]])
+    }
+    assert res == exp
