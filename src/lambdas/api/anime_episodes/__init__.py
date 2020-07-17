69452ee235fe15371c905b713824f14a5c4ce9f5
fulder <misad90@gmail.com>
2020-07-17 12:21:13 +0200

    Add support for limit/start for get_episodes

--- src/lambdas/api/anime_episodes/__init__.py
+++ src/lambdas/api/anime_episodes/__init__.py
@@ -15,9 +15,16 @@ def handle(event, context):
     log.debug(f"Received event: {event}")
 
     anime_id = event["pathParameters"].get("id")
+    limit = 100
+    start = 1
+
+    query_params = event.get("queryStringParameters")
+    if query_params:
+        limit = query_params.get("limit")
+        start = query_params.get("start")
 
     try:
-        res = episodes_db.get_episodes(anime_id)
+        res = episodes_db.get_episodes(anime_id, limit=limit, start=start)
     except episodes_db.NotFoundError:
         log.debug(f"No episodes found for anime with id: {anime_id}")
         return {"statusCode": 404}
