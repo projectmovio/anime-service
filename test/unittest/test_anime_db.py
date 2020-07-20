d1a4eacd3622a0be2881270cdcc60602f1757468
fulder <misad90@gmail.com>
2020-07-20 11:19:44 +0200

    Return anime_id together with 202 code from post

--- test/unittest/test_anime_db.py
+++ test/unittest/test_anime_db.py
@@ -9,6 +9,7 @@ def test_get_annime_by_mal_id_not_found(mocked_anime_db):
     with pytest.raises(mocked_anime_db.NotFoundError):
         mocked_anime_db.get_anime_by_mal_id("123")
 
+
 def test_get_annime_not_found(mocked_anime_db):
     mocked_anime_db.table.query.return_value = {
         "Items": []
@@ -16,4 +17,3 @@ def test_get_annime_not_found(mocked_anime_db):
 
     with pytest.raises(mocked_anime_db.NotFoundError):
         mocked_anime_db.get_anime("123")
-
