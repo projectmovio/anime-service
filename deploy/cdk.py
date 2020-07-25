39d1cbce5a789386d84466b84ba164b929fdb920
fulder <misad90@gmail.com>
2020-07-25 17:57:00 +0200

    Fix bug with ANIDB_CLIENT environment var being not set

--- deploy/cdk.py
+++ deploy/cdk.py
@@ -17,7 +17,7 @@ if mal_client_id is None:
     raise RuntimeError("Please set the MAL_CLIENT_ID environment variable")
 
 anidb_client = os.getenv("ANIDB_CLIENT")
-if mal_client_id is None:
+if anidb_client is None:
     raise RuntimeError("Please set the ANIDB_CLIENT environment variable")
 
 domain_name = os.getenv("DOMAIN_NAME")
--- deploy/lib/anime.py
+++ deploy/lib/anime.py
@@ -6,7 +6,7 @@ from aws_cdk import core
 from aws_cdk.aws_apigateway import DomainName
 from aws_cdk.aws_apigatewayv2 import HttpApi, HttpMethod, CfnAuthorizer, CfnRoute, \
     HttpIntegration, HttpIntegrationType, PayloadFormatVersion, CfnStage
-from aws_cdk.aws_certificatemanager import Certificate, ValidationMethod, CertificateValidation
+from aws_cdk.aws_certificatemanager import Certificate, ValidationMethod
 from aws_cdk.aws_dynamodb import Table, Attribute, AttributeType, BillingMode
 from aws_cdk.aws_events import Rule, Schedule
 from aws_cdk.aws_events_targets import LambdaFunction
@@ -359,3 +359,5 @@ class Anime(core.Stack):
             ),
             stage_name="live"
         )
+
+
