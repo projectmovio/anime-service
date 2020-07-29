c9bbf29cfaa216096f7a3b102e22ee97396bc90e
fulder <misad90@gmail.com>
2020-07-29 23:25:25 +0200

    Fix tests after API changes

--- test/unittest/test_anime_db.py
+++ test/unittest/test_anime_db.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-def test_get_annime_by_mal_id_not_found(mocked_anime_db):
+def test_get_anime_by_mal_id_not_found(mocked_anime_db):
     mocked_anime_db.table.query.return_value = {
         "Items": []
     }
@@ -10,10 +10,10 @@ def test_get_annime_by_mal_id_not_found(mocked_anime_db):
         mocked_anime_db.get_anime_by_mal_id("123")
 
 
-def test_get_annime_not_found(mocked_anime_db):
+def test_get_anime_not_found(mocked_anime_db):
     mocked_anime_db.table.query.return_value = {
         "Items": []
     }
 
-    with pytest.raises(mocked_anime_db.NotFoundError):
-        mocked_anime_db.get_anime("123")
+    res = mocked_anime_db.get_anime("123")
+    assert res == {}
