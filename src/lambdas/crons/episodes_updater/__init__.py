16141c4e02b8e018e3cde8c708fb97a0285df74c
fulder <misad90@gmail.com>
2020-08-16 10:44:55 +0200

    Add logs to new crons lambda

--- src/lambdas/crons/episodes_updater/__init__.py
+++ src/lambdas/crons/episodes_updater/__init__.py
@@ -22,8 +22,10 @@ def _get_sqs_queue():
 
 def handle(event, context):
     day_of_week = datetime.today().strftime('%A').lower()
+    log.info(f"Updating anime with broadcast_day: {day_of_week}")
 
     for anime in anime_db.anime_by_broadcast_generator(day_of_week):
+        log.debug(f"Sending SQS message for anime with id: ${anime['id']}")
         _get_sqs_queue().send_message(
             MessageBody=json.dumps({
                 "mal_id": anime["mal_id"],
