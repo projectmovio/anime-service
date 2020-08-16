4cc963df4716cc674f14f988d91494962bd77e66
fulder <misad90@gmail.com>
2020-08-16 10:39:52 +0200

    Implement episode updater lambda

---
+++ src/lambdas/crons/episodes_updater/__init__.py
@@ -0,0 +1,33 @@
+import json
+from datetime import datetime
+
+import boto3
+
+import logger
+import anime_db
+from api.anime import _get_sqs_queue
+
+log = logger.get_logger("episode_updater")
+
+SQS_QUEUE_URL = os.getenv("POST_ANIME_SQS_QUEUE_URL")
+
+sqs_queue = None
+
+
+def _get_sqs_queue():
+    global sqs_queue
+    if sqs_queue is None:
+        sqs_queue = boto3.resource("sqs").Queue(SQS_QUEUE_URL)
+    return sqs_queue
+
+
+def handle(event, context):
+    day_of_week = datetime.today().strftime('%A').lower()
+
+    for anime in anime_db.anime_by_broadcast_generator(day_of_week):
+        _get_sqs_queue().send_message(
+            MessageBody=json.dumps({
+                "mal_id": anime["mal_id"],
+                "force_update": True
+            })
+        )
