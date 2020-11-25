a693e10e546e98b6b2ef25f6b7544ab883c09afe
fulder <misad90@gmail.com>
2020-11-25 21:26:41 +0100

    Return next and previous episode ID links

--- src/layers/databases/python/episodes_db.py
+++ src/layers/databases/python/episodes_db.py
@@ -1,5 +1,6 @@
 import os
 import uuid
+from datetime import datetime, date
 
 import boto3
 from boto3.dynamodb.conditions import Key
@@ -70,7 +71,16 @@ def get_episode(anime_id, episode_id):
     if res["Count"] != 1:
         raise InvalidAmountOfEpisodes(f"Episode with ID: {episode_id} and anime_id: {anime_id} has {res['Count']} results")
 
-    return res["Items"][0]
+    episode_data = res["Items"][0]
+    episode_data["id_links"] = {}
+
+    if "episode_number" in episode_data and episode_data["episode_number"] != 1:
+        episode_data["id_links"]["previous"] = _create_episode_uuid(anime_id, episode_data["episode_number"] - 1)
+
+    if "air_date" in episode_data and datetime.strptime(episode_data["air_date"], '%Y-%m-%d') <= date.today():
+        episode_data["id_links"]["next"] = _create_episode_uuid(anime_id, episode_data["episode_number"] + 1)
+
+    return episode_data
 
 
 def get_episodes(anime_id, limit=100, start=1):
