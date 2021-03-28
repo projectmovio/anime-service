b9a772eac4a61190fd5693790b69ccd7fe18eeb1
fulder <misad90@gmail.com>
2021-03-28 22:58:17 +0200

    Add support for getting episode by api id

--- src/lambdas/api/anime_episodes/__init__.py
+++ src/lambdas/api/anime_episodes/__init__.py
@@ -15,10 +15,35 @@ def handle(event, context):
     log.debug(f"Received event: {event}")
 
     anime_id = event["pathParameters"].get("id")
+    query_params = event.get("queryStringParameters")
+
+    if "api_id" in query_params:
+        return _get_episode_by_api_id(anime_id, query_params)
+    else:
+        return _get_episodes(anime_id, query_params)
+
+
+def _get_episode_by_api_id(anime_id, query_params):
+    if not query_params:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Please specify query parameters"})
+        }
+
+    if "anidb_id" in query_params:
+        try:
+            res = episodes_db.get_episode_by_api_id("anidb", int(query_params["anidb_id"]))
+            return {"statusCode": 200, "body": json.dumps(res, cls=decimal_encoder.DecimalEncoder)}
+        except (episodes_db.NotFoundError, episodes_db.InvalidAmountOfEpisodes):
+            return {"statusCode": 404}
+    else:
+        return {"statusCode": 400, "body": json.dumps({"error": "Unsupported query param"})}
+
+
+def _get_episodes(anime_id, query_params):
     limit = 100
     start = 1
 
-    query_params = event.get("queryStringParameters")
     if query_params and "limit" in query_params:
         limit = query_params.get("limit")
     if query_params and "start" in query_params:
